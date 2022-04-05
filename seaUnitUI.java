1b30f1ddf50a4366c24c0d5aa8e28d0aca9e55a6
TangyFish <92803369+TangyFish@users.noreply.github.com>
2022-04-05 08:45:23 -0500

    Add files via upload

--- seaUnitUI.java
+++ seaUnitUI.java
@@ -1,9 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 
 public class seaUnitUI extends UnitUI{
 	
-	public seaUnitUI(seaUnit u, boolean m, boolean s)
+	seaUnit selectedUnit;
+	boolean moveButtonPressed;
+	boolean supportButtonPressed;
+	boolean convoyButtonPressed;
+	
+	
+	public seaUnitUI(seaUnit u)
 	{
 		super();
+		moveButtonPressed = false;
+		supportButtonPressed = false;
+		convoyButtonPressed = false;
 		
+		final JButton move = new JButton("Move Unit");
+		move.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				moveButtonPressed = true;
+				dispose();
+			}	
+		});
+		
+		final JButton support = new JButton("Support");
+		support.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				supportButtonPressed = true;
+				dispose();
+			}
+		
+		});
+		
+		final JButton convoy = new JButton("Convoy");
+		support.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				convoyButtonPressed = true;
+				dispose();
+			}
+		
+		});
+		
+		this.add(move, BorderLayout.CENTER);
+		this.add(support, BorderLayout.WEST);
+		this.add(convoy, BorderLayout.EAST);
 	}
+	
+	
 }
